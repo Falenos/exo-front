29e2d4c6bdd6433cae86675786e32d7c7a88d02c
Georgios Kakavas <Georgios.Kakavas@gmail.com>
2019-01-13 14:25:10 +0200

    Added html styles assets and dev build configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const globImporter = require('node-sass-glob-importer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const paths = {
@@ -14,31 +15,61 @@ module.exports = {
     app: path.resolve(paths.src , 'js/app.js')
   },
   output: {
-		path: path.resolve(paths.root, "dist"),
-		filename: "bundle.js",
+		path: path.resolve(paths.root, 'dist'),
+		filename: 'bundle.js',
   },
 	plugins: [new HtmlWebpackPlugin({
-    title: "",
-    template: path.resolve(paths.src, "html/index.html"),
-
+    title: 'Exozet front-end',
+    template: path.resolve(paths.src, 'html/index.html'),
   })],
+  devtool: 'inline-cheap-module-source-map',
   resolve: {
     modules: ['node_modules'],
     extensions: ['.js'],
     symlinks: false,
   },
-
   module: {
     rules: [].concat(
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader'
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'assets/images',
+            name: '[name].[ext]',
+          },
+        },
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: 'assets/fonts',
+            name: '[name].[ext]',
+          },
+        },
+      },
 			{
 				test: /\.s?css$/,
 				include: [
 					paths.src,
 				],
 				use: [
-					{ loader: "style-loader" },
-					{ loader: "css-loader" },
-					{ loader: "sass-loader" }
+					{ loader: 'style-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'sass-loader',
+            options: {
+              importer: globImporter()
+            }
+          }
 				],
 			}
     )
